c066dd66e51fee73e86c872abea52db32dc130f7
Yuri do Monte <yspm.developer@gmail.com>
2022-07-16 22:29:49 -0300

    colecao-array

---
+++ advanced-php-array-string-functions-web/funcoes.php
@@ -0,0 +1,36 @@
+<?php
+
+function sacar(array $conta, float $valorASacar): array{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não tem saldo suficiente");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+
+    return $conta;
+}
+
+function exibeMensagem(string $mensagem){
+    echo $mensagem . '<br>';
+}
+
+function depositar(array $conta, float $valorADepositar): array{
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Depositos precisam ser positivos");
+    }
+    return $conta;
+}
+
+//& passagem por referencia
+function titularComLetrasMaiusculas(array &$conta){
+    $conta['titular'] = mb_strtoupper($conta['titular']);
+    return $conta;
+    
+}
+
+function exibeConta(array $conta){
+    ['titular' => $titular, 'saldo' => $saldo] = $conta;
+    echo "<li>Titular: $titular . Saldo: $saldo</li>";
+}
